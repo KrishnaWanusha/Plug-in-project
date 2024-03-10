7ac02c7858eee330e47eeca07638be5eba8c2283
KrishnaWanusha <krishnawanusha@gmail.com>
2024-03-10 12:04:01 +0530

    Initial Commit

---
+++ MobileShopProductProducer/src/mobileshopproductproducer/MobileShopProductService.java
@@ -0,0 +1,14 @@
+package mobileshopproductproducer;
+
+import java.util.List;
+
+import productmodel.Product;
+
+public interface MobileShopProductService {
+
+	void addProduct(String brand, String model, String year, int ram, int storage, double retailPrice, int stock);
+	List<Product> getProducts();
+	void restockProduct(Product product, int qty);
+	void viewCatalogue();
+	Product getProductById(String id);
+}
